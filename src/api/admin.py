dc1c26c7862e12ffc54869927721648029c8aa5b
Mikhail <mikhail0234@gmail.com>
2017-09-26 19:46:46 +0300

    Added models and admin panel

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from .models import Categories, Dish
+
+class CategoriesAdmin(admin.ModelAdmin):
+     list_display = ["name"]
+     class Meta:
+         model = Categories
+
+
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["title", "get_category", "price"]
+
+    def get_category(self, obj):
+        return obj.category.name
+
+    class Meta:
+        model = Dish
+
+
+admin.site.register(Categories, CategoriesAdmin)
+
+admin.site.register(Dish, DishAdmin)
--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=40)
+
+def __str__(self):
+    return '%s' % (self.name)
+
+class Dish(models.Model):
+    title = models.CharField(max_length=40)
+    price = models.IntegerField()
+    category = models.ForeignKey(Categories)
+
+def __str__(self):
+    return '%s %d %s' % (self.title, self.price, self.category)
+
+
